bf4762b7a54828abe85888b42c026c73dd0e583b
bdotgeorge <bzzz2009@yandex.ru>
2023-06-07 21:00:56 +0400

    home work done

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -1,18 +1,28 @@
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class HomeWork {
-    public void first(){
+    public void first() {
         int i = generateIntInRange(0, 2000);
         int n = highBitNumber(i);
-        System.out.println(n);
-        System.out.println(i);
+        int[] m1 = multiplicity(i, n, true);
+        int[] m2 = multiplicity(i, n, false);
 
+        System.out.println("Number bit = " + n + '\n' + "Number = " + i + '\n');
+        for (int j : m1) {
+            System.out.print("m1 = " + j + " ");
+        }
+        System.out.println('\n');
+        for (int j : m2) {
+            System.out.print("m2 = " + j + " ");
+        }
     }
-    private int generateIntInRange(int min, int max){
+
+    private int generateIntInRange(int min, int max) {
         return new Random().nextInt(min, max);
     }
-    private int highBitNumber(int value){
+
+    private int highBitNumber(int value) {
         int c = 0;
         while (1 < value) {
             value >>= 1;
@@ -20,10 +30,19 @@ public class HomeWork {
         }
         return c;
     }
-    private int[] multiplicity(int from, int n, boolean multiples){
-        List<int> result;
-        for (int i = from; i < Short.MAX_VALUE; i++) {
-            if(i % n == 0) result.add(i)
+
+    private int[] multiplicity(int from, int n, boolean multiples) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (multiples) {
+            for (int i = from; i < Short.MAX_VALUE; i++) {
+                if (i % n == 0) result.add(i);
+            }
+        } else {
+            for (int j = Short.MIN_VALUE; j < from; j++) {
+                if (j % n != 0) result.add(j);
+            }
         }
+        int[] value = result.stream().mapToInt(Integer::intValue).toArray();
+        return value;
     }
 }
